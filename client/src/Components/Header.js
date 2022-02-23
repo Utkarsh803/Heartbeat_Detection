4106757bfbcaefe074e36cc1b367008a07d95963
Utkarsh Gupta <guptau@tcd.ie>
2022-02-23 18:44:26 +0000

    Added a Header Component for the page

---
+++ client/src/Components/Header.js
@@ -0,0 +1,35 @@
+import React, {Component, useState, useEffect} from "react";
+import {useContext} from 'react';
+import '../css/Header.css'
+import logo from '../assets/mslogo.jpg'
+
+
+const Header = () => {
+    // Sticky Menu Area
+    useEffect(() => {
+      window.addEventListener("scroll", isSticky);
+      return () => {
+        window.removeEventListener("scroll", isSticky);
+      };
+    });
+  
+    /* Method that will fix header after a specific scrollable */
+    const isSticky = (e) => {
+      const header = document.querySelector(".header");
+      const scrollTop = window.scrollY;
+      scrollTop >= 250
+        ? header.classList.add("is-sticky")
+        : header.classList.remove("is-sticky");
+    };
+
+
+    return (
+      <>
+        <header className="header">
+        <img src={logo}></img>
+        </header>
+      </>
+    );
+  };
+  
+  export default Header;
