307da4317027b15eee8754c514ae150900e96416
Utkarsh Gupta <guptau@tcd.ie>
2022-03-01 17:19:13 +0000

    Added img component for video feed

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,12 @@ const App = () => {
   return (
     <div className="homepage">
       <div> <Header/></div>
+      <div className="video">
+   <img
+    src="http://127.0.0.1:3001/video_feed"
+    alt="Video"
+   />
+  </div>
       <div style={{ marginLeft: (window.innerWidth / 2) + 10, marginTop: '90px', width: (window.innerWidth / 2) - 40 }}>
       <Box bgcolor="#f4f4f4" boxShadow="5px 5px 5px #b2b2b3" p={50} borderRadius="borderRadius">
       </Box>
