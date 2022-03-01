0d94f38360e6dd7e65373bdad1f7cc7fb420ebec
Niall Connolly <connoln4@tcd.ie>
2022-03-01 10:34:03 +0000

    Box added to screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from './Components/Header';
+import Box from '@material-ui/core/Box';
+
 
 
 const App = () => {
@@ -8,7 +10,12 @@ const App = () => {
   return (
     <div className="homepage">
       <div> <Header/></div>
+      <div style={{ marginLeft: window.innerWidth / 2, marginTop: '90px', width: (window.innerWidth / 2) - 20, height: window.innerHeight }}>
+      <Box color="white" bgcolor="darkgrey" p={50}>
+      </Box>
+      </div>
     </div>
+
    );
   };
 
