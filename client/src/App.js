51c7c6ca9e242b6fe7c4d363bd753d85b7346a83
Niall Connolly <connoln4@tcd.ie>
2022-03-03 10:51:35 +0000

    box changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,12 @@ const App = () => {
   return (
     <div className="homepage">
       <div> <Header/></div>
+      <div className="video">
+        <img
+          src="http://127.0.0.1:3001/video_feed"
+          alt="Video"
+        />
+      </div>
       <Box className="glassBox" borderRadius="borderRadius">
       </Box>
     </div>
