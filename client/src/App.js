269385d314b54e0c591dc9e5976747c3128ff76d
Niall Connolly <connoln4@tcd.ie>
2022-03-06 20:30:10 +0000

    added camera

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './App.css';
 import Header from './Components/Header';
 import Box from '@material-ui/core/Box';
-
+import Camera from './Components/Camera';
 
 
 const App = () => {
-
   return (
     <div className="homepage">
-      <div> <Header/></div>
-      <div className="video">
-        <img
-          src="http://127.0.0.1:3001/video_feed"
-          alt="Video"
-        />
-      </div>
-      <Box className="glassBox" borderRadius="borderRadius">
-      </Box>
+      <div><Header/></div>
+      <div><Camera/></div>
+      <Box className="glassBox" borderRadius="borderRadius"/>
     </div>
    );
   };
 
 export default App;
+
