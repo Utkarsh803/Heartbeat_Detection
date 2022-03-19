1bb4be69b6f476ea4b1aecb7229fb6d1321fd651
Utkarsh Gupta <guptau@tcd.ie>
2022-03-19 00:08:48 +0000

    Displayed logged in username on header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,9 +4,10 @@ import '../css/Header.css'
 import logo from '../assets/mslogo.jpg'
 import '../css/CameraToggle.scss'
 import CameraToggle from "./CameraToggle";
-
-
-
+import { callMsGraph } from "../graph";
+import { ProfileData } from "./ProfileData";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "../authConfig";
 
 const Header = () => {
     // Sticky Menu Area
@@ -27,12 +28,42 @@ const Header = () => {
     };
 
 
+function ProfileContent() {
+  const { instance, accounts } = useMsal();
+  const [graphData, setGraphData] = useState(null);
+
+  const name = accounts[0] && accounts[0].name;
+function RequestProfileData() {
+      const request = {
+          ...loginRequest,
+          account: accounts[0]
+      };
+
+      // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+      instance.acquireTokenSilent(request).then((response) => {
+          callMsGraph(response.accessToken).then(response => setGraphData(response));
+      }).catch((e) => {
+          instance.acquireTokenPopup(request).then((response) => {
+              callMsGraph(response.accessToken).then(response => setGraphData(response));
+          });
+      });
+  }
+
+  return (
+      <>
+          <span>Welcome {name}</span>
+      </>
+  );
+};
+
+
     return (
       <>
         <header className="header">
         <img src={logo}></img>
+        <div className="username"><ProfileContent /></div>
         <button  className="button">
-          Save Data
+          Sign Out
         </button>
         <camera-toggle className="camera-toggle"><CameraToggle></CameraToggle></camera-toggle>
         </header>
